5c2198b5c5df32d1e2804201f9b0aa0fc78662e6
Kimchi-Haggis <kimchihaggis@gmail.com>
2022-08-19 13:02:15 +0100

    [Feat] SEO title

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,15 @@ import type { NextPage } from 'next'
 
 import Weather from '@components/weather';
 import ToggleTheme from '@components/toggleDarkMode/ToggleTheme';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
 
   return (
     <div>
+      <Head>
+        <title>Weather App | ChangYeol Lee</title>
+      </Head>
       <Weather />
       <ToggleTheme />
     </div>
